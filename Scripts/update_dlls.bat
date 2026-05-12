6d7ce92c869d4d9b9174035da4f67e3717e85264
Matt Furnari <mattfurnari@hotmail.com>
2026-05-12 14:45:23 +0000

    Add partner dependency binaries for ProtoScript build

--- Scripts/update_dlls.bat
+++ Scripts/update_dlls.bat
@@ -1,20 +1,18 @@
 call "%~dp0set_params.bat"
 set LIB_ROOT=%ROOT%\lib\
 set DEPLOY_ROOT=%ROOT%\Deploy\
-set ONTOLOGY_DEPLOY=%ROOT%\..\ontology-core\Deploy\
+set ONTOLOGY_DEPLOY=%ROOT%\..\ontology\Deploy\
 
-xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\BasicUtilities.dll %LIB_ROOT%*.*
-xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\WebAppUtilities.dll %LIB_ROOT%*.*
+if exist "c:\dev\RooTrax\RooTrax.Utilities\Deploy\BasicUtilities.dll" xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\BasicUtilities.dll %LIB_ROOT%*.*
+if exist "c:\dev\RooTrax\RooTrax.Utilities\Deploy\WebAppUtilities.dll" xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\WebAppUtilities.dll %LIB_ROOT%*.*
 
-xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Cache.dll %LIB_ROOT%*.*
-xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.dll %LIB_ROOT%*.*
-xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.DB.dll %LIB_ROOT%*.*
+if exist "c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Cache.dll" xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Cache.dll %LIB_ROOT%*.*
+if exist "c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.dll" xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.dll %LIB_ROOT%*.*
+if exist "c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.DB.dll" xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.DB.dll %LIB_ROOT%*.*
 
-xcopy /y C:\dev\Buffaly.Development\Deploy\CSharp.dll %LIB_ROOT%*.*
-xcopy /y C:\dev\Buffaly.Development\Deploy\CSharp.Parsers.dll %LIB_ROOT%*.*
+if exist "C:\dev\Buffaly.Development\Deploy\CSharp.dll" xcopy /y C:\dev\Buffaly.Development\Deploy\CSharp.dll %LIB_ROOT%*.*
+if exist "C:\dev\Buffaly.Development\Deploy\CSharp.Parsers.dll" xcopy /y C:\dev\Buffaly.Development\Deploy\CSharp.Parsers.dll %LIB_ROOT%*.*
 
 if exist "%ONTOLOGY_DEPLOY%Ontology.dll" xcopy /y "%ONTOLOGY_DEPLOY%Ontology*.dll" "%DEPLOY_ROOT%*.*"
 
-
-
-
+exit /b 0
